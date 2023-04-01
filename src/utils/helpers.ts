9becd36d48ca6e3e0f91d17fb0d4ded124c08205
levanthoi <lethoi1506@gmail.com>
2023-04-01 14:04:26 +0700

    fix ProductPrice

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,9 +1,15 @@
 export const formatNumber = (locale: string, number: number) => {
   let format;
-  if (locale === 'vi') {
-    format = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(number);
-  } else {
-    format = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(number);
+  if (locale) {
+    if (locale === 'vi') {
+      format = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(
+        number,
+      );
+    } else {
+      format = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
+        number,
+      );
+    }
   }
   return format;
 };
